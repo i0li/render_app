4e3ba54994c03aec0569db921fe7adee9f021813
iori <iori.sa1013@icloud.com>
2023-03-07 18:50:46 +0900

    fix auth structure

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,9 +50,10 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'name' => ['required', 'string', 'max:10'],
+            'login_id' => ['required', 'string', 'max:10', 'unique:users'],
+            'password' => ['required', 'string', 'max:60', 'confirmed'],
+            'role_id' => ['required', 'max:1']
         ]);
     }
 
@@ -66,8 +67,9 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'email' => $data['email'],
+            'login_id' => $data['login_id'],
             'password' => Hash::make($data['password']),
+            'role_id' => $data['role_id']
         ]);
     }
 }
