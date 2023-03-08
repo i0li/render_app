61c2c9a7dc81fd9d6fb2cfd908bad8c653ba32a3
iori <iori.sa1013@icloud.com>
2023-03-08 16:44:13 +0900

    fix js for pusher

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,27 +30,3 @@ window.Echo = new Echo({
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     forceTLS: true
 });
-
-window.Echo.channel('ChatChannel')
-.listen('SendChatEvent',function(data){
-
-  if((data['chat_contants']['user_id'] != login_user_id) && (data['chat_contants']['room_id'] == current_room_id)){
-    let receive_textarea_value_array = data['chat_contants']['text'].split(/\r?\n/g);
-    let receive_chat_contants = '<div class="message_box w-100"> \
-                                  <div class="d-flex flex-row ms-3 mt-3 text-secondary align-items-center pb-1"> \
-                                    <img src="' + data['chat_contants']['user_icon'] + '" class="border rounded-circle img-responsive me-2" width="30px">\
-                                    <div class="pe-2">'+ data['chat_contants']['user_name'] +'</div>\
-                                    <div class="pe-2">' + data['chat_contants']['send_at'] + '</div>\
-                                    <div class="pe-2"></div>\
-                                  </div>\
-                                  <div class="bg-white ms-3 px-3 py-2 alert alert-secondary message-box">';
-    $.each(receive_textarea_value_array, function(index, value){
-      receive_chat_contants += value+'</br>';
-    })
-    receive_chat_contants += '</div> </div>';
-  
-    $('#chat_area_contants').append(receive_chat_contants)
-    let chat_area_contants = $('#chat_area_contants');
-    chat_area_contants[0].scrollTo(0, chat_area_contants[0].scrollHeight);
-  }
-});
