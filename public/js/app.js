ff255d230f2ea3d2703f702b8ff96e23e37a1b27
iori <iori.sa1013@icloud.com>
2023-03-09 11:26:48 +0900

    fix pusher_id in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -5433,7 +5433,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "222740652ce7ac6a6a0d",
+  key: "d6b826dad24ece430e39",
   cluster: "ap3",
   forceTLS: true
 });
